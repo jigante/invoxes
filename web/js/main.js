0731e1f14a166f394e188f15c5cdceadb9367ae8
Cristiano Santeramo <cristiano@agileweb.it>
2013-04-17 20:31:52 +0100

    Aggiunto il codice js per permettere di attivare i metodi "DELETE" e "PUT" sui link contenenti l'attributo "data-href-method"

--- web/js/main.js
+++ web/js/main.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
 
+    $('a[data-href-method]').on('click', function (e){
+        var link = $(this),
+        href = link.attr('href'),
+        method = link.data('href-method'),
+        form = $('<form method="post" action="'+href+'">'),
+        metadata_input = '<input name="_method" value="'+method+'" type="hidden" />';
+
+        // if (csrf_param != null && csrf_token != null) {
+        //     metadata_input += '<input name="'+csrf_param+'" value="'+csrf_token+'" type="hidden" />';
+        // }
+
+        form.hide()
+            .append(metadata_input)
+            .appendTo('body');
+
+        e.preventDefault();
+
+        confirmMessage = link.data('mconfirm-header');
+        
+        if (typeof confirmMessage === 'undefined' || confirmMessage === false) {
+            confirmMessage = "Are you sure?";
+        }
+
+        var confirmResult = confirm(confirmMessage);
+
+        if (confirmResult) {
+            form.submit();
+        }
+    });
+
 });
