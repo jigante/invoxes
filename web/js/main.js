4660b6020775905a46569c68251b65f9b3928545
Cristiano Santeramo <cristiano@agileweb.it>
2013-04-17 22:40:01 +0100

    Encapsulated function to submit via PUT/DELETE methods a link

--- web/js/main.js
+++ web/js/main.js
@@ -1,33 +1,33 @@
+var links = {
+    // If there is a link with the data attribute "data-href-method"
+    // We build an hidden form to submit the link using the "_method" defined in it
+    submitByHrefMethod: function() {
+        $('a[data-href-method]').on('click', function (e){
+            var link = $(this),
+            href = link.attr('href'),
+            method = link.data('href-method'),
+            form = $('<form method="post" action="'+href+'">'),
+            metadata_input = '<input name="_method" value="'+method+'" type="hidden" />';
+
+            form.hide()
+                .append(metadata_input)
+                .appendTo('body');
+
+            e.preventDefault();
+
+            confirmMessage = link.data('mconfirm-header');
+
+            if (typeof confirmMessage === 'undefined' || confirmMessage === false) {
+                confirmMessage = "Are you sure?";
+            }
+
+            if (confirm(confirmMessage)) {
+                form.submit();
+            }
+        });
+    }
+};
+
 $(document).ready(function() {
-
-    $('a[data-href-method]').on('click', function (e){
-        var link = $(this),
-        href = link.attr('href'),
-        method = link.data('href-method'),
-        form = $('<form method="post" action="'+href+'">'),
-        metadata_input = '<input name="_method" value="'+method+'" type="hidden" />';
-
-        // if (csrf_param != null && csrf_token != null) {
-        //     metadata_input += '<input name="'+csrf_param+'" value="'+csrf_token+'" type="hidden" />';
-        // }
-
-        form.hide()
-            .append(metadata_input)
-            .appendTo('body');
-
-        e.preventDefault();
-
-        confirmMessage = link.data('mconfirm-header');
-        
-        if (typeof confirmMessage === 'undefined' || confirmMessage === false) {
-            confirmMessage = "Are you sure?";
-        }
-
-        var confirmResult = confirm(confirmMessage);
-
-        if (confirmResult) {
-            form.submit();
-        }
-    });
-
+    links.submitByHrefMethod();
 });
