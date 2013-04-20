7ce466c3431af83d7af77781f5f237a767554185
Cristiano Santeramo <cristiano@agileweb.it>
2013-04-20 08:37:33 +0100

    - Added entity and repository class for "Contact" - Generated migration for table contact

---
+++ src/Agile/InvoiceBundle/Entity/Contact.php
@@ -0,0 +1,254 @@
+<?php
+
+namespace Agile\InvoiceBundle\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Contact
+ *
+ * @ORM\Table(name="contact")
+ * @ORM\Entity(repositoryClass="Agile\InvoiceBundle\Entity\ContactRepository")
+ */
+class Contact
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="first_name", type="string", length=100)
+     * @Assert\NotBlank();
+     * @Assert\Length(min=3, max=100)
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="last_name", type="string", length=100)
+     * @Assert\NotBlank();
+     * @Assert\Length(min=3, max=100)
+     */
+    private $lastName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=100, nullable=true)
+     * @Assert\Length(min=2, max=100)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=100, nullable=true)
+     * @Assert\Email(message="The email '{{value}}' is not a valid email.", checkMX = true, checkHost = true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="phone_office", type="string", length=20, nullable=true)
+     * @Assert\Length(min=3, max=20)
+     */
+    private $phoneOffice;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="phone_mobile", type="string", length=20, nullable=true)
+     * @Assert\Length(min=3, max=20)
+     */
+    private $phoneMobile;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="fax", type="string", length=20, nullable=true)
+     * @Assert\Length(min=3, max=20)
+     */
+    private $fax;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     * @return Contact
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string 
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     * @return Contact
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string 
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Contact
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Contact
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set phoneOffice
+     *
+     * @param string $phoneOffice
+     * @return Contact
+     */
+    public function setPhoneOffice($phoneOffice)
+    {
+        $this->phoneOffice = $phoneOffice;
+    
+        return $this;
+    }
+
+    /**
+     * Get phoneOffice
+     *
+     * @return string 
+     */
+    public function getPhoneOffice()
+    {
+        return $this->phoneOffice;
+    }
+
+    /**
+     * Set phoneMobile
+     *
+     * @param string $phoneMobile
+     * @return Contact
+     */
+    public function setPhoneMobile($phoneMobile)
+    {
+        $this->phoneMobile = $phoneMobile;
+    
+        return $this;
+    }
+
+    /**
+     * Get phoneMobile
+     *
+     * @return string 
+     */
+    public function getPhoneMobile()
+    {
+        return $this->phoneMobile;
+    }
+
+    /**
+     * Set fax
+     *
+     * @param string $fax
+     * @return Contact
+     */
+    public function setFax($fax)
+    {
+        $this->fax = $fax;
+    
+        return $this;
+    }
+
+    /**
+     * Get fax
+     *
+     * @return string 
+     */
+    public function getFax()
+    {
+        return $this->fax;
+    }
+}
