29bb2866d73a4aca01794c631fc06e08a44f3a05
Cristiano Santeramo <cristiano@agileweb.it>
2013-05-05 15:08:58 +0100

    Added additional fields "first_name", "last_name", "company", "contact_phone" to user entity

--- src/Agile/InvoiceBundle/Entity/User.php
+++ src/Agile/InvoiceBundle/Entity/User.php
@@ -4,6 +4,8 @@ namespace Agile\InvoiceBundle\Entity;
 
 use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 /**
  * @ORM\Entity
@@ -11,6 +13,12 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class User extends BaseUser
 {
+    // public function __construct()
+    // {
+    //     parent::__construct();
+    //     // your own logic
+    // }
+
     /**
      * @ORM\Id
      * @ORM\Column(type="integer")
@@ -18,9 +26,133 @@ class User extends BaseUser
      */
     protected $id;
 
-    public function __construct()
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="first_name", type="string", length=100)
+     * @Assert\NotBlank(groups={"Registration", "Profile"});
+     * @Assert\Length(min=3, max=100, groups={"Registration", "Profile"})
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="last_name", type="string", length=100)
+     * @Assert\NotBlank(groups={"Registration", "Profile"});
+     * @Assert\Length(min=3, max=100, groups={"Registration", "Profile"})
+     */
+    private $lastName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="company", type="string", length=100)
+     * @Assert\Length(min=5, max=100, groups={"Registration"})
+     * @Assert\NotBlank(groups={"Registration"});
+     */
+    private $company;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="contact_phone", type="string", length=20)
+     * @Assert\NotBlank(groups={"Registration", "Profile"});
+     * @Assert\Length(min=3, max=20, groups={"Registration", "Profile"})
+     */
+    private $contactPhone;
+
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     * @return Account
+     */
+    public function setFirstName($firstName)
     {
-        parent::__construct();
-        // your own logic
+        $this->firstName = $firstName;
+    
+        return $this;
     }
+
+    /**
+     * Get firstName
+     *
+     * @return string 
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     * @return Account
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string 
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set company
+     *
+     * @param string $company
+     * @return Account
+     */
+    public function setCompany($company)
+    {
+        $this->company = $company;
+    
+        return $this;
+    }
+
+    /**
+     * Get company
+     *
+     * @return string 
+     */
+    public function getCompany()
+    {
+        return $this->company;
+    }
+
+    /**
+     * Set contactPhone
+     *
+     * @param string $contactPhone
+     * @return Account
+     */
+    public function setContactPhone($contactPhone)
+    {
+        $this->contactPhone = $contactPhone;
+    
+        return $this;
+    }
+
+    /**
+     * Get contactPhone
+     *
+     * @return string 
+     */
+    public function getContactPhone()
+    {
+        return $this->contactPhone;
+    }
+
 }
