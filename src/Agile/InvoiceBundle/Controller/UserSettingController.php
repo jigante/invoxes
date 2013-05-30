ae3feac604cbd034703925bb28214c0c4b503ea4
Cristiano Santeramo <cristiano@agileweb.it>
2013-05-30 11:29:00 +0200

    Added the controller "UserSettingController" to set via Ajax the user settings

---
+++ src/Agile/InvoiceBundle/Controller/UserSettingController.php
@@ -0,0 +1,41 @@
+<?php 
+
+namespace Agile\InvoiceBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * User Controller
+ * @Route("/users/settings")
+ */
+class UserSettingController extends Controller
+{
+
+    /**
+    * @Route("/{user}/{settingName}/{settingValue}", name="set_user_setting", options={"expose"=true})
+    */
+    public function setUserSetting($settingName, $settingValue)
+    {
+        $user = $this->getUser();
+        $request = $this->getRequest();
+
+        // Is it an Ajax request?
+        if ($request->isXmlHttpRequest()) {
+            // Set the user setting
+            $em = $this->getDoctrine()->getManager();
+            $setting = $em->getRepository('AgileInvoiceBundle:UserSetting')->setUserSetting(
+                $user, $settingName, $settingValue
+            );
+
+            return Response::create('Ok');
+        } else {
+            throw $this->createNotFoundException();
+        }
+        
+    }
+
+}
