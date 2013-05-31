e1cdf9143f683add8b439af11fd29becf6e25f9b
Cristiano Santeramo <cristiano@agileweb.it>
2013-05-31 17:42:21 +0200

    Removed the method "isDisabledWelcome()" because now it is inside the entity for better application logic

--- src/Agile/InvoiceBundle/Entity/UserRepository.php
+++ src/Agile/InvoiceBundle/Entity/UserRepository.php
@@ -6,13 +6,5 @@ use Doctrine\ORM\EntityRepository;
 
 class UserRepository extends EntityRepository
 {
-    public function isDisabledWelcome($user)
-    {
-        $settingName = 'disable_welcome_screen';
-        $em = $this->getEntityManager();
 
-        $isDisabledWelcome = $em->getRepository('AgileInvoiceBundle:UserSetting')->isDisabled($user, $settingName);
-
-        return $isDisabledWelcome;
-    }
 }
