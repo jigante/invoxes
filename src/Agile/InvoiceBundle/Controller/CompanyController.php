4d9613b314006d15e78de20ecc59b4748232009b
Cristiano Santeramo <cristiano@agileweb.it>
2013-06-04 18:58:55 +0200

    Created the empty controller "CompanyController"

---
+++ src/Agile/InvoiceBundle/Controller/CompanyController.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace Agile\InvoiceBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * Company Controller.
+ * 
+ * @Route("/company")
+ */
+class CompanyController extends Controller
+{
+
+}
