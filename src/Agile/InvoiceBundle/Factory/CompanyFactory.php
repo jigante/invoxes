f27c8883fc177d76b3a385a244d86ff018893286
Cristiano Santeramo <cristiano@agileweb.it>
2013-06-06 16:56:15 +0200

    Added functionality to switching context for admin users

--- src/Agile/InvoiceBundle/Factory/CompanyFactory.php
+++ src/Agile/InvoiceBundle/Factory/CompanyFactory.php
@@ -3,21 +3,33 @@
 namespace Agile\InvoiceBundle\Factory;
 
 use Agile\InvoiceBundle\Entity\User;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Doctrine\Common\Persistence\ObjectManager;
 
 class CompanyFactory
 {
     private $user;
+    private $session;
+    private $entity_manager;
 
-    public function __construct(User $user = null)
+    public function __construct(User $user = null, Session $session, ObjectManager $entityManager)
     {
         $this->user = $user;
+        $this->session = $session;
+        $this->entityManager = $entityManager;
     }
 
     public function get()
     {
         $company = null;
 
-        if ($this->user) {
+        if ($companyId = $this->session->get('context.company_id')) {
+            $company = $this->entity_manager
+                ->getRepository('AgileInvoiceBundle:Company')
+                ->find($companyId);
+        }
+
+        if (!$company && $this->user) {
             $company = $this->user->getCompany();
         }
 
