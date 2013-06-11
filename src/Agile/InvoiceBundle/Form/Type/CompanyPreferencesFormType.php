c1f0bd8a6cc220c3821a369763d0bae3bc41d977
Cristiano Santeramo <cristiano@agileweb.it>
2013-06-12 00:06:16 +0200

    Build the first draft of the Company preferences form

--- src/Agile/InvoiceBundle/Form/Type/CompanyPreferencesFormType.php
+++ src/Agile/InvoiceBundle/Form/Type/CompanyPreferencesFormType.php
@@ -4,27 +4,52 @@ namespace Agile\InvoiceBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionResolver\OptionResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityRepository;
 
 class CompanyPreferencesFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $company = $options['data'];
         $builder
-            ->add('name', 'text', array('label' => 'form.company_name', 'translation_domain' => 'AgileInvoiceBundle'))
+            ->add('name', 'text', array('label' => 'company.name', 'translation_domain' => 'AgileInvoiceBundle'))
+            ->add('owner', 'entity', array(
+                'required' => true,
+                'label' => 'company.account_owner',
+                'translation_domain' => 'AgileInvoiceBundle',
+                'class' => 'AgileInvoiceBundle:User',
+                'property' => 'choicheListName',
+                'query_builder' => function(EntityRepository $er) use ($company) {
+                    return $er->createQueryBuilder('u')
+                        ->orderBy('u.firstName', 'ASC')
+                        ->where('u.company = :company')
+                        ->setParameter('company', $company)
+                    ;
+                }
+            ))
+            ->add('fiscalYearStart', null, array('label' => 'company.fiscal_year', 'translation_domain' => 'AgileInvoiceBundle'))
+            ->add('timezone', null, array('label' => 'company.timezone', 'translation_domain' => 'AgileInvoiceBundle'))
+            ->add('dateFormat', null, array('label' => 'company.date_format', 'translation_domain' => 'AgileInvoiceBundle'))
+            ->add('currency', null, array('label' => 'company.currency', 'translation_domain' => 'AgileInvoiceBundle'))
+            ->add('currencyPlacement', null, array('label' => 'company.currency_placement', 'translation_domain' => 'AgileInvoiceBundle'))
+            ->add('includeCurrencyCode', null, array('label' => 'company.include_currency_code', 'translation_domain' => 'AgileInvoiceBundle'))
+            ->add('numberFormat', null, array('label' => 'company.number_format', 'translation_domain' => 'AgileInvoiceBundle'))
+            ->add('colorScheme', null, array('label' => 'company.color_scheme', 'translation_domain' => 'AgileInvoiceBundle'))
+            ->add('logo', null, array('label' => 'company.logo', 'translation_domain' => 'AgileInvoiceBundle'))
         ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Agile\InvoiceBundle\Entity\Company',
+        ));
     }
 
     public function getName()
     {
         return 'agile_invoice_company_preferences';
-    }
-
-    public function setDefaultOptions(OptionResolverInterface $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => 'Agile\InvoiceBundle\Entity\Company';
-        ));
     }
 
 }
