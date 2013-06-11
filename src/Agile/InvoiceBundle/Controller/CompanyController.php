ae4c64acd548c5e624d3ed2cab3f9db105b53ab4
Cristiano Santeramo <cristiano@agileweb.it>
2013-06-12 00:01:23 +0200

    Create actions "accountAction()", "preferencesAction()", "preferencesUpdateAction()"

--- src/Agile/InvoiceBundle/Controller/CompanyController.php
+++ src/Agile/InvoiceBundle/Controller/CompanyController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Agile\InvoiceBundle\Form\Type\CompanyPreferencesFormType;
 
 /**
  * Company Controller.
@@ -16,4 +17,41 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class CompanyController extends Controller
 {
 
+    /**
+     * @Route("/account", name="company_account")
+     * @Template()
+     */
+    public function accountAction()
+    {
+        $company = $this->get('context.company');
+
+        return array(
+            'company' => $company,
+        );
+    }
+
+    /**
+     * @Route("/preferences", name="company_preferences")
+     * @Template()
+     */
+    public function preferencesAction()
+    {
+        $company = $this->get('context.company');
+
+        $form = $this->createForm(new CompanyPreferencesFormType(), $company);
+
+        return array(
+            'form' => $form->createView(),
+        );
+    }
+
+    /**
+     * @Route("/preferences", name="company_preferences_update")
+     * @Method("PUT")
+     * @Template("AgileInvoiceBundle:Company:preferences.html.twig")
+     */
+    public function preferencesUpdateAction(Request $request)
+    {
+        # code...
+    }
 }
