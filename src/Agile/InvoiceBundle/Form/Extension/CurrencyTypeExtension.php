661c7d977f5436ceebe8605d6b5cff2bd24bc89a
Cristiano Santeramo <cristiano@agileweb.it>
2013-07-05 07:47:46 +0100

    Added the visualization of the default company currency when creating a new client

--- src/Agile/InvoiceBundle/Form/Extension/CurrencyTypeExtension.php
+++ src/Agile/InvoiceBundle/Form/Extension/CurrencyTypeExtension.php
@@ -3,12 +3,20 @@
 namespace Agile\InvoiceBundle\Form\Extension;
 
 use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Agile\InvoiceBundle\Utility\Utils as Utility;
 use Symfony\Component\Intl\Intl;
 
 class CurrencyTypeExtension extends AbstractTypeExtension
 {
+    private $translator;
+
+    public function __construct($translator)
+    {
+        $this->translator = $translator;
+    }
+
     /**
      * Returns the name of the type being extended.
      *
@@ -24,15 +32,20 @@ class CurrencyTypeExtension extends AbstractTypeExtension
      */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
+
         $resolver->setDefaults(array(
-            'choices' => $this->getCurrencyChoices(),
+            'choices' => function (Options $options) {
+                return $this->getCurrencyChoices($options['view_account_default_currency']);
+            },
             'preferred_choices' => array(
                 'EUR', 'GBP', 'USD', 'CAD', 'AUD', 'NZD', 'SEK', 'DKK', 'NOK', 'CHF', 'ZAR'
             ),
+            'view_account_default_currency' => null,
         ));
+
     }
 
-    private function getCurrencyChoices()
+    private function getCurrencyChoices($viewAccountDefaultCurrency = null)
     {
         $currenySymbols = Utility::getCurrencySymbols();
         
@@ -47,7 +60,16 @@ class CurrencyTypeExtension extends AbstractTypeExtension
         foreach ($currenySymbols as $symbol) {
             $currencyName = Intl::getCurrencyBundle()->getCurrencyName($symbol);
             if ($currencyName) {
-                $currencyChoices[$symbol] = $currencyName.' - '.$symbol;
+                $currencyValue = $currencyName.' - '.$symbol;
+                if ($viewAccountDefaultCurrency == $symbol) {
+                    $currencyChoices[$symbol] = $this->translator->trans(
+                        'account.default.%currencyValue%',
+                        array('%currencyValue%' => $currencyValue)
+                    );
+                } else {
+                    $currencyChoices[$symbol] = $currencyValue;
+                }
+                
             }
         }
 
