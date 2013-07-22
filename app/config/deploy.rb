a6b894db3ef21020b4a4145aff5e73c830a65edb
Cristiano Santeramo <cristiano@agileweb.it>
2013-07-22 12:08:23 +0100

    Updated "deploy.rb" so that now deployment is working

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,11 +1,16 @@
-set :application, "set your application name here"
-set :domain,      "#{application}.com"
-set :deploy_to,   "/var/www/#{domain}"
+set :application, "Fatture Facili"
+# set :domain,      "#{application}.com"
+set :domain,      "fatturefacili.it"
+# set :deploy_to,   "/var/www/#{domain}"
+set :deploy_to,   "/var/www/fatturefacili.it/application"
 set :app_path,    "app"
+# set :web_path, "web"
 
-set :repository,  "#{domain}:/var/repos/#{application}.git"
+# SCM info
+# set :repository,  "#{domain}:/var/repos/#{application}.git"
+set :repository,  "git@github.com:jigante/fatture-online.git"
 set :scm,         :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+set :deploy_via, :remote_cache
 
 set :model_manager, "doctrine"
 # Or: `propel`
@@ -15,5 +20,15 @@ role :app,        domain, :primary => true       # This may be the same as your 
 
 set  :keep_releases,  3
 
+# General config stuff
+set :shared_files,      ["app/config/parameters.yml"]
+set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor"]
+set :use_composer, true
+
+# User details for the production server
+set :user, "fatturefacili"
+set :use_sudo, false
+ssh_options[:port] = 7822
+
 # Be more verbose by uncommenting the following line
-# logger.level = Logger::MAX_LEVEL
+logger.level = Logger::MAX_LEVEL
